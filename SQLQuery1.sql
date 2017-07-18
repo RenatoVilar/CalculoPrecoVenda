f9b3b642efa30a41a8aa6a313dab2e1ce7ed6496
Renato Vilar <renatovilar@msn.com>
2017-07-18 14:20:02 -0400

    Remove MVVM Framework not used

--- SQLQuery1.sql
+++
@@ -1,6 +0,0 @@
-﻿INSERT INTO Ncms (CodNcm, NomeNcm, ImpImportacao, Ipi) VALUES ('90148010', 'SONDA', 1, 2);
-INSERT INTO Ncms (CodNcm, NomeNcm, ImpImportacao, Ipi) VALUES ('80108014', 'RADIO', 5, 2);
-INSERT INTO Ncms (CodNcm, NomeNcm, ImpImportacao, Ipi) VALUES ('60000000', 'RADAR', 1, 2);
-INSERT INTO Ncms (CodNcm, NomeNcm, ImpImportacao, Ipi) VALUES ('80801400', 'ANTENA', 1, 2);
-
-SELECT * FROM Ncms
--- SQLQuery3.sql
+++
@@ -1,7 +0,0 @@
-﻿INSERT INTO 
-	CalculoPrecoVendaDb.dbo.Ufs 
-	(NomeUf, SiglaUf, AliquotaInterna, AliquotaFcp, ItensFcp, AliquotaInterestadual, AliquotaEmbarcacoes)
-SELECT 
-	NomeUf, SiglaUf, AliquotaInterna, AliquotaFcp, ItensFcp, AliquotaInterestadual, AliquotaEmbarcacoes
-FROM
-	[C:\USERS\RENATO.ALEGRANAUTICA\DOCUMENTS\VISUAL STUDIO 2015\PROJECTS\CALCULOPRECOVENDA\CALCULOPRECOVENDA\DATA\OLD.MDF].dbo.Ufs
